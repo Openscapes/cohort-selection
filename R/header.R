0605b01ad62e509c470b767390e665399cb06bea
Andy Teucher <andy.teucher@gmail.com>
2024-09-26 10:24:09 -0700

    Make functions and header file in R/

---
+++ R/header.R
@@ -0,0 +1,15 @@
+gs4_auth(email = "andy@openscapes.org")
+
+signup_sheet <- "10zTz6i-ZuwnQhCTG63jRku0BtVwgc9e1tqnfGuiyM2c"
+signup_sheet_mentor_cp <- "1z8VWGExea-0X2olPVJINLXgimcweWIUMAuI7mU6UYKg"
+
+os_mainlist <- "10ub0NKrPa1phUa_X-Jxg8KYH57WGLaZzBN-vQT4e10o"
+
+# Worksheet for annotation by mentors
+annotated_ws <- "annotated-signups_2024-09-23"
+
+# Final worksheet for fine-tuning selection
+final_worksheet <- "final-selection_2024-09-25"
+
+# Summary worksheet
+summary_sheet <- paste0("summary-", final_worksheet)
