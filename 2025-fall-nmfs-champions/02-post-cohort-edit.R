9c413ae7f3ecfd1dc077f7eada5ceb81ff2f63f8
Andy Teucher <andy.teucher@gmail.com>
2025-09-22 13:56:42 -0700

    Simplify summaries to just summarize post-edit sheet

--- 2025-fall-nmfs-champions/02-post-cohort-edit.R
+++ 2025-fall-nmfs-champions/02-post-cohort-edit.R
@@ -8,38 +8,8 @@ source("R/functions.R")
 gs4_auth(email = "andy@openscapes.org")
 
 signup_sheet <- "1fpMJpReMuAW3W_U7rGSBkM1BgzWdVySLqLec-xoZYUc"
-os_mainlist <- "10ub0NKrPa1phUa_X-Jxg8KYH57WGLaZzBN-vQT4e10o"
 
-ss_raw <- read_signup_sheet(signup_sheet)
-
-team_lookup <- read_sheet(signup_sheet, sheet = "Notes")
-
-prev_participants <- read_sheet(os_mainlist) |>
-  mutate(email = tolower(email))
-
-ss_edited <- read_sheet(signup_sheet, sheet = "cohort-picks", range = "A1:R113")
-
-ss <- ss_raw |>
-  # Join to previous participants sheet so we can flag them
-  left_join(
-    prev_participants |>
-      select(email, cohort) |>
-      group_by(email) |>
-      summarise(prev_cohort = paste(cohort, collapse = " / ")),
-    by = c("email_address" = "email"),
-    na_matches = "never"
-  ) |>
-  mutate(
-    prev_champion = !is.na(prev_cohort)
-  ) |>
-  mutate(
-    division = stringr::str_replace(division, "\\s*\\(.*\\)", ""),
-    parent_division = stringr::str_split_i(division, "[- /]", 1)
-  ) |>
-  left_join(team_lookup, by = "team_name") |>
-  mutate(across(starts_with("cohort_"), \(x) {
-    ifelse(grepl("yes", x), "yes", x)
-  }))
+ss_edited <- read_sheet(signup_sheet, sheet = "cohort-picks", range = "A1:R116")
 
 cohort_summary <- ss_edited |>
   rename(cohort = cohort_pick) |>
@@ -75,16 +45,7 @@ ss_edited |>
   count(parent_division, cohort_pick) |>
   View()
 
-ss |>
-  left_join(
-    ss_edited |>
-      select(email_address, team_name, cohort_pick),
-    by = "email_address"
-  ) |>
-  filter(team_name.x != team_name.y) |>
-  View()
-
-# sheet_delete(ss = signup_sheet, sheet = "post-edit-summaries")
+sheet_delete(ss = signup_sheet, sheet = "post-edit-summaries")
 sheet_add(ss = signup_sheet, sheet = "post-edit-summaries")
 
 range_write(
